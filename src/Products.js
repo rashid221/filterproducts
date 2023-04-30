2dc4f7c79db8fa89b57ea0b1c46792f98d137df1
rashid <rashidsahil1@gmail.com>
2023-04-30 23:38:58 +0530

    changes done after the filter method

--- src/Products.js
+++ src/Products.js
@@ -9,59 +9,81 @@ const Products = () => {
   const dispatch = useDispatch();
   return (
     <div>
-      <h4 style={{textAlign:'center',backgroundColor:'#212A3E',padding:'1rem',color:'white'}}>Products Availability</h4>
+      <h4
+        style={{
+          textAlign: "center",
+          backgroundColor: "#212A3E",
+          padding: "1rem",
+          color: "white",
+          margin: 0,
+        }}
+      >
+        Products Availability
+      </h4>
 
-    <div style={{display:'flex',flexDirection:'row',width:'100%'}}>
-      
-       {
-        <div style={{flex:1}}>
-         {datas.map((item) => (
-            <ul key={item.id}  style={{listStyleType:'none'}}>
-                <li className="li-hover" onClick={() => dispatch(sendData(item.category))} >{item.category}</li>
-            </ul>
-          ))}
+      <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+        {
+          <div style={{ flex: 1 }}>
+            <h4
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                padding: "1rem 0.5rem",
+                margin: 0,
+              }}
+            >
+              Search by filter
+            </h4>
+            {datas.map((item) => (
+              <ul key={item.id} style={{ listStyleType: "none" }}>
+                <li
+                  className="li-hover"
+                  onClick={() => dispatch(sendData(item.category))}
+                >
+                  {item.category}
+                </li>
+              </ul>
+            ))}
           </div>
-       }
-       
+        }
 
-
-      {filtered.length === 0 ? 
-        <div className="cards-main">
-          {datas.map((item) => (
-            <div className="cards">
-            <img key={item.id}
-              src={item.image}
-              alt=""
-              style={{ width: "5rem" }}
-             
-            />
-            <h5>{item.title}</h5>
-            <h5>Price: {item.price}</h5>
-</div>
-          ))}
-        </div>
-       : 
-      
-        <div className="cards-main">
-          
-          {filtered.map((ite) => (
-            ite.map((im)=>
-            <div className="cards">
-            <img key={im.id}
-              src={im.image}
-              alt=""
-              style={{ width: "5rem" }}
-             
-            />
-            <h5>{im.title}</h5>
-            <h5>Price: {im.price}</h5>
-</div>  
-            )
-            // <img src={ite.image} alt="" />
-          ))}
-        </div>
-      }
-    </div>
+        {filtered.length === 0 ? (
+          <div className="cards-main">
+            {datas.map((item) => (
+              <div className="cards">
+                <img
+                  key={item.id}
+                  src={item.image}
+                  alt=""
+                  style={{ width: "5rem" }}
+                />
+                <h5>{item.title}</h5>
+                <h5>Price: ₹ {item.price}</h5>
+                <h5>Rating <span className="span-rate">{item.rating.rate} ★</span></h5>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="cards-main">
+            {filtered.map((ite) =>
+              ite.map((im) => (
+                <div className="cards">
+                  <img
+                    key={im.id}
+                    src={im.image}
+                    alt=""
+                    style={{ width: "5rem" }}
+                  />
+                  <h5>{im.title}</h5>
+                  <h5>Price: ₹ {im.price}</h5>
+                  <h5>Rating <span className="span-rate">{im.rating.rate} ★</span></h5>
+                </div>
+              ))
+              // <img src={ite.image} alt="" />
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,32 +1,26 @@
 import { actionTypes } from "../types/types";
 
-
 const initialState = {
-    products:[],
-    filteredProducts:[]
+  products: [],
+  filteredProducts: [],
 };
 
-export const productReducer = (state=initialState,action) =>{
-    console.log(action);
-        switch(action.type){
-            case actionTypes.SET_PRODUCTS:
-                
-                return {
-                    ...state,
-                    products:action.payload
-                }
-                case actionTypes.SEND_DATA:
-                    let productId = action.payload;
-                    let x = state.products.filter((item)=>item.category === productId);
-                    return {
-                        ...state,
-                        filteredProducts:[x]
-                        
-                           
-                    }
-                default :
-                        return state
-                            
-        }
-
-} 
+export const productReducer = (state = initialState, action) => {
+  console.log(action);
+  switch (action.type) {
+    case actionTypes.SET_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case actionTypes.SEND_DATA:
+      let productId = action.payload;
+      let x = state.products.filter((item) => item.category === productId);
+      return {
+        ...state,
+        filteredProducts: [x],
+      };
+    default:
+      return state;
+  }
+};
